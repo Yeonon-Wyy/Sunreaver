e7161e1b9add06fc916951d3a9ec44ae36600af8
Weiyanyu <chinayeonon@gmail.com>
2019-10-09 23:26:16 +0800

    init repo

---
+++ src/main/java/top/yeonon/yclouddisk/YCloudDiskApplication.java
@@ -0,0 +1,13 @@
+package top.yeonon.yclouddisk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YCloudDiskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YCloudDiskApplication.class, args);
+    }
+
+}
