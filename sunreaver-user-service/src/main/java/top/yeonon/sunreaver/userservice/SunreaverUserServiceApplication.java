c0ffdf1b6c1b18556a39e73ed8b73cb09839a837
Weiyanyu <chinayeonon@gmail.com>
2019-10-27 17:30:30 +0800

    add nacos config

--- sunreaver-user-service/src/main/java/top/yeonon/sunreaver/userservice/SunreaverUserServiceApplication.java
+++ sunreaver-user-service/src/main/java/top/yeonon/sunreaver/userservice/SunreaverUserServiceApplication.java
@@ -3,13 +3,18 @@ package top.yeonon.sunreaver.userservice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import top.yeonon.sunreaver.userservice.auth.AuthInterceptor;
 
 @SpringBootApplication
+@RefreshScope
 @ComponentScan(basePackages = "top.yeonon.sunreaver")
+@EnableDiscoveryClient
 public class SunreaverUserServiceApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
